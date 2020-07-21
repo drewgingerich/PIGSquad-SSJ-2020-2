1269ef17b54115d8045bd85541372067e0e7aa40
Drew Gingerich <gingerich93@gmail.com>
2020-07-21 16:47:38 -0700

    Add a folder for general scripts and assets

---
+++ Assets/General/Follow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+	[Range(0, 100)]
+	public float stiffness = 10f;
+	public Transform target;
+	public Vector3 scale = new Vector3(1, 1, 0);
+
+	const float stiffnessScaler = 0.01f;
+
+	void Update()
+	{
+		Vector3 diff = target.position - transform.position;
+		Vector3 scaledDiff = Vector3.Scale(diff, scale);
+		transform.Translate(scaledDiff * stiffness * stiffnessScaler);
+	}
+}
