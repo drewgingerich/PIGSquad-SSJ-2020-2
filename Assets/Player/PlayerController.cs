7cd1c6e0410a18285a0cbb8a55ecfacb4339c366
Drew Gingerich <gingerich93@gmail.com>
2020-07-21 16:46:11 -0700

    Add simple character controller

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[Header("Tweaks")]
+	[SerializeField]
+	private float speed = 2f;
+	[SerializeField]
+	private float mass = 5f;
+	[SerializeField]
+	private float dashSpeed = 2f;
+	[SerializeField]
+	private float dashDuration = 2f;
+
+	[Header("Refs")]
+	[SerializeField]
+	private LineRenderer beam;
+	[SerializeField]
+	private MuteSourceDriver moveAudioSource;
+	[SerializeField]
+	private MuteSourceDriver dashAudioSource;
+	[SerializeField]
+	private MuteSourceDriver shootAudioSource;
+
+	private PlayerInputActions controls;
+	private Vector2 velocity = Vector2.zero;
+	private Vector2 moveInput;
+	private Vector2 aimInput;
+	private bool dashInput = false;
+	private bool shootInput = false;
+
+	private bool isDashing = false;
+	private bool isShooting = false;
+
+	private ContactFilter2D hitFilter = new ContactFilter2D();
+	private RaycastHit2D[] hitBuffer = new RaycastHit2D[1];
+
+	private void OnEnable()
+	{
+		if (controls == null)
+		{
+			controls = new PlayerInputActions();
+			controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+			controls.Player.Dash.performed += ctx => dashInput = true;
+			controls.Player.Aim.performed += ctx =>
+			{
+				var screenPoint = ctx.ReadValue<Vector2>();
+				aimInput = Camera.main.ScreenToWorldPoint(screenPoint);
+			};
+			controls.Player.Shoot.performed += ctx => shootInput = true;
+		}
+		controls.Player.Enable();
+
+		StopAllCoroutines();
+		StartCoroutine(MoveState());
+	}
+
+	private void Update()
+	{
+		if (shootInput && !isShooting)
+		{
+			moveAudioSource.Stop();
+			dashAudioSource.Stop();
+			isDashing = false;
+			StopAllCoroutines();
+			StartCoroutine(ShootState());
+		}
+		if (dashInput && !isDashing)
+		{
+			isShooting = false;
+			moveAudioSource.Stop();
+			StopAllCoroutines();
+			StartCoroutine(DashState());
+		}
+	}
+
+	private void OnDisable()
+	{
+		controls.Player.Disable();
+	}
+
+	private IEnumerator IdleState()
+	{
+		velocity = Vector2.zero;
+		while (true)
+		{
+			if (moveInput != Vector2.zero)
+			{
+				StartCoroutine(MoveState());
+				break;
+			}
+			yield return null;
+		}
+	}
+
+	private IEnumerator MoveState()
+	{
+		moveAudioSource.Play();
+		while (true)
+		{
+			bool stopped = Vector2.Distance(velocity, Vector2.zero) < 0.05f;
+			bool noInput = moveInput == Vector2.zero;
+			if (stopped && noInput)
+			{
+				moveAudioSource.Stop();
+				StartCoroutine(IdleState());
+				break;
+			}
+
+			Vector2 moveDiff = moveInput - velocity;
+			velocity += moveDiff * Time.deltaTime * mass;
+			transform.Translate(velocity * speed);
+			yield return null;
+		}
+	}
+
+	private IEnumerator DashState()
+	{
+		dashAudioSource.Play();
+		dashInput = false;
+		isDashing = true;
+		Vector2 direction = velocity.normalized;
+		float timer = 0f;
+		while (true)
+		{
+			timer += Time.deltaTime;
+			if (timer >= dashDuration)
+			{
+				isDashing = false;
+				dashAudioSource.Stop();
+				StartCoroutine(MoveState());
+				break;
+			}
+			transform.Translate(direction * dashSpeed * Time.deltaTime);
+			yield return null;
+		}
+	}
+
+	private IEnumerator ShootState()
+	{
+		shootInput = false;
+		isShooting = true;
+		Vector2 direction = (aimInput - (Vector2)transform.position).normalized;
+		var hits = Physics2D.Raycast(transform.position, direction, hitFilter, hitBuffer);
+		var endPoint = hits > 0
+			? (Vector3)hitBuffer[0].point
+			: transform.position + (Vector3)direction * 100;
+		var beamPositions = new Vector3[]{
+				transform.position,
+				endPoint
+			};
+		beam.SetPositions(beamPositions);
+		beam.enabled = true;
+		shootAudioSource.Play();
+		yield return new WaitForSeconds(0.25f);
+		beam.enabled = false;
+		isShooting = false;
+		StartCoroutine(IdleState());
+	}
+
+	private IEnumerator Shoot(Vector2 direction)
+	{
+		var hits = Physics2D.Raycast(transform.position, direction, hitFilter, hitBuffer);
+		var endPoint = hits > 0
+			? (Vector3)hitBuffer[0].point
+			: transform.position + (Vector3)direction * 100;
+		var beamPositions = new Vector3[]{
+				transform.position,
+				endPoint
+			};
+		beam.SetPositions(beamPositions);
+		beam.enabled = true;
+		yield return new WaitForSeconds(0.25f);
+		beam.enabled = false;
+	}
+}
---
+++ Assets/Player/PlayerInputActions.cs
@@ -0,0 +1,303 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Player/PlayerInputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""42b97063-6878-470e-b46b-6650d15fbde5"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""52262f6b-b20f-4dea-be71-5977fc28bdac"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""42bc82cd-597b-4b76-a05d-ea6b6831e1cc"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Reload"",
+                    ""type"": ""Button"",
+                    ""id"": ""39dd06d0-9ec3-4432-b59a-3dcad3a76505"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Aim"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""312d1903-bcbc-4450-8d0c-70b71bc28717"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Shoot"",
+                    ""type"": ""Button"",
+                    ""id"": ""0fe277aa-ae12-4037-9005-ff2bcef6543a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""18ddc956-4499-424c-91c7-103530b8d24d"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""0c1e3032-f178-46c3-ab83-175a25702404"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""a4716724-035b-4739-83d7-fd535408bfee"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""fe33d9c4-9bdc-4cdb-829a-1eb6e1d233d0"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""9a23d165-eb79-4a8c-9967-4f2ceea203d9"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""40f46225-47d5-4516-8283-edbf86873853"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9d36d97d-e77d-47c8-a7d7-ba3babfdd74d"",
+                    ""path"": ""<Keyboard>/f"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Reload"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""64ca7bb8-b84c-47a9-9740-dc6c6ca4d8da"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Reload"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""61cf86f6-8fdb-4393-a0be-6113f971dd03"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Aim"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""052ac214-17ba-44bd-9fc4-a51c6c6f9da9"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Shoot"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Dash = m_Player.FindAction("Dash", throwIfNotFound: true);
+        m_Player_Reload = m_Player.FindAction("Reload", throwIfNotFound: true);
+        m_Player_Aim = m_Player.FindAction("Aim", throwIfNotFound: true);
+        m_Player_Shoot = m_Player.FindAction("Shoot", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Dash;
+    private readonly InputAction m_Player_Reload;
+    private readonly InputAction m_Player_Aim;
+    private readonly InputAction m_Player_Shoot;
+    public struct PlayerActions
+    {
+        private @PlayerInputActions m_Wrapper;
+        public PlayerActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @Dash => m_Wrapper.m_Player_Dash;
+        public InputAction @Reload => m_Wrapper.m_Player_Reload;
+        public InputAction @Aim => m_Wrapper.m_Player_Aim;
+        public InputAction @Shoot => m_Wrapper.m_Player_Shoot;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Dash.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Reload.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnReload;
+                @Reload.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnReload;
+                @Reload.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnReload;
+                @Aim.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAim;
+                @Aim.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAim;
+                @Aim.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAim;
+                @Shoot.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @Shoot.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @Shoot.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Dash.started += instance.OnDash;
+                @Dash.performed += instance.OnDash;
+                @Dash.canceled += instance.OnDash;
+                @Reload.started += instance.OnReload;
+                @Reload.performed += instance.OnReload;
+                @Reload.canceled += instance.OnReload;
+                @Aim.started += instance.OnAim;
+                @Aim.performed += instance.OnAim;
+                @Aim.canceled += instance.OnAim;
+                @Shoot.started += instance.OnShoot;
+                @Shoot.performed += instance.OnShoot;
+                @Shoot.canceled += instance.OnShoot;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnDash(InputAction.CallbackContext context);
+        void OnReload(InputAction.CallbackContext context);
+        void OnAim(InputAction.CallbackContext context);
+        void OnShoot(InputAction.CallbackContext context);
+    }
+}
