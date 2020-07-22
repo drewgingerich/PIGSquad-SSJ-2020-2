a5105eb3ba54eb1f5f854b8ae005a7b0b5c94ad9
Drew Gingerich <gingerich93@gmail.com>
2020-07-22 16:05:55 -0700

    Fix logic error in wait for note yield instruction

--- Assets/Audio/WaitForNote.cs
+++ Assets/Audio/WaitForNote.cs
@@ -2,21 +2,18 @@
 
 public class WaitForNote : CustomYieldInstruction
 {
-	private Note noteType;
 	private double targetTime;
-	private int offset;
 
 	public override bool keepWaiting
 	{
 		get
 		{
-			return targetTime < AudioSettings.dspTime;
+			return targetTime > AudioSettings.dspTime;
 		}
 	}
 
 	public WaitForNote(Note noteType, int offset = 0)
 	{
-		this.noteType = noteType;
 		targetTime = NoteTracker.GetNextNoteTime(noteType, offset);
 	}
 }
