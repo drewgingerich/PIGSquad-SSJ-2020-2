4a74532f3f742b92e26caed909d6d45baf467b05
Drew Gingerich <gingerich93@gmail.com>
2020-07-21 21:20:09 -0700

    Refactor player controller to use a state machine

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -16,6 +16,8 @@ public class PlayerController : MonoBehaviour
 
 	[Header("Refs")]
 	[SerializeField]
+	private NoteTracker noteTracker;
+	[SerializeField]
 	private LineRenderer beam;
 	[SerializeField]
 	private MuteSourceDriver moveAudioSource;
@@ -23,6 +25,8 @@ public class PlayerController : MonoBehaviour
 	private MuteSourceDriver dashAudioSource;
 	[SerializeField]
 	private MuteSourceDriver shootAudioSource;
+
+	private StateMachine fsm = new StateMachine();
 
 	private PlayerInputActions controls;
 	private Vector2 velocity = Vector2.zero;
@@ -33,47 +37,47 @@ public class PlayerController : MonoBehaviour
 
 	private bool isDashing = false;
 	private bool isShooting = false;
+	private float dashTimer;
 
 	private ContactFilter2D hitFilter = new ContactFilter2D();
 	private RaycastHit2D[] hitBuffer = new RaycastHit2D[1];
 
-	private void OnEnable()
-	{
-		if (controls == null)
-		{
-			controls = new PlayerInputActions();
-			controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-			controls.Player.Dash.performed += ctx => dashInput = true;
-			controls.Player.Aim.performed += ctx =>
-			{
-				var screenPoint = ctx.ReadValue<Vector2>();
-				aimInput = Camera.main.ScreenToWorldPoint(screenPoint);
-			};
-			controls.Player.Shoot.performed += ctx => shootInput = true;
-		}
-		controls.Player.Enable();
+	private const int STATE_IDLE = 0;
+	private const int STATE_MOVE = 1;
+	private const int STATE_DASH = 2;
+	private const int STATE_SHOOT = 3;
+	private const int STATE_RELOAD = 4;
 
-		StopAllCoroutines();
-		StartCoroutine(MoveState());
+	#region LIFECYCLE
+
+	private void Awake()
+	{
+		fsm.AddState(STATE_IDLE, EnterIdleState, UpdateIdleState, null);
+		fsm.AddState(STATE_MOVE, EnterMoveState, UpdateMoveState, null);
+		fsm.AddState(STATE_DASH, EnterDashState, UpdateDashState, ExitDashState);
+		fsm.AddState(STATE_SHOOT, EnterShootState, null, ExitShootState);
 	}
 
-	private void Update()
+	private void OnEnable()
 	{
-		if (shootInput && !isShooting)
+		if (controls == null) WireControls();
+
+		controls.Player.Enable();
+
+		fsm.ChangeToState(STATE_IDLE);
+	}
+
+	private void WireControls()
+	{
+		controls = new PlayerInputActions();
+		controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+		controls.Player.Dash.performed += ctx => dashInput = true;
+		controls.Player.Aim.performed += ctx =>
 		{
-			moveAudioSource.Stop();
-			dashAudioSource.Stop();
-			isDashing = false;
-			StopAllCoroutines();
-			StartCoroutine(ShootState());
-		}
-		if (dashInput && !isDashing)
-		{
-			isShooting = false;
-			moveAudioSource.Stop();
-			StopAllCoroutines();
-			StartCoroutine(DashState());
-		}
+			var screenPoint = ctx.ReadValue<Vector2>();
+			aimInput = Camera.main.ScreenToWorldPoint(screenPoint);
+		};
+		controls.Player.Shoot.performed += ctx => shootInput = true;
 	}
 
 	private void OnDisable()
@@ -81,67 +85,100 @@ public class PlayerController : MonoBehaviour
 		controls.Player.Disable();
 	}
 
-	private IEnumerator IdleState()
+	private void Update()
+	{
+		if (shootInput && !isShooting)
+		{
+			fsm.ChangeToState(STATE_SHOOT);
+		}
+		else if (dashInput && !isDashing)
+		{
+			fsm.ChangeToState(STATE_DASH);
+		}
+		fsm.Update();
+	}
+
+	#endregion
+
+	#region STATE_IDLE
+
+	private void EnterIdleState()
 	{
 		velocity = Vector2.zero;
-		while (true)
-		{
-			if (moveInput != Vector2.zero)
-			{
-				StartCoroutine(MoveState());
-				break;
-			}
-			yield return null;
-		}
 	}
 
-	private IEnumerator MoveState()
+	private void UpdateIdleState()
+	{
+		if (moveInput != Vector2.zero) fsm.ChangeToState(STATE_MOVE);
+	}
+
+	#endregion
+
+	#region STATE_MOVE
+
+	private void EnterMoveState()
 	{
 		moveAudioSource.Play();
-		while (true)
-		{
-			bool stopped = Vector2.Distance(velocity, Vector2.zero) < 0.05f;
-			bool noInput = moveInput == Vector2.zero;
-			if (stopped && noInput)
-			{
-				moveAudioSource.Stop();
-				StartCoroutine(IdleState());
-				break;
-			}
-
-			Vector2 moveDiff = moveInput - velocity;
-			velocity += moveDiff * Time.deltaTime * mass;
-			transform.Translate(velocity * speed);
-			yield return null;
-		}
 	}
 
-	private IEnumerator DashState()
+	private void UpdateMoveState()
+	{
+		bool stopped = Vector2.Distance(velocity, Vector2.zero) < 0.05f;
+		bool noInput = moveInput == Vector2.zero;
+		if (stopped && noInput) fsm.ChangeToState(STATE_IDLE);
+
+		Vector2 moveDiff = moveInput - velocity;
+		velocity += moveDiff * Time.deltaTime * mass;
+		transform.Translate(velocity * speed);
+	}
+
+	private void ExitMoveState()
+	{
+		moveAudioSource.Stop();
+	}
+
+	#endregion
+
+	#region STATE_DASH
+
+	private void EnterDashState()
 	{
 		dashAudioSource.Play();
 		dashInput = false;
 		isDashing = true;
-		Vector2 direction = velocity.normalized;
-		float timer = 0f;
-		while (true)
-		{
-			timer += Time.deltaTime;
-			if (timer >= dashDuration)
-			{
-				isDashing = false;
-				dashAudioSource.Stop();
-				StartCoroutine(MoveState());
-				break;
-			}
-			transform.Translate(direction * dashSpeed * Time.deltaTime);
-			yield return null;
-		}
+		dashTimer = 0f;
 	}
 
-	private IEnumerator ShootState()
+	private void UpdateDashState()
+	{
+		Vector2 direction = velocity.normalized;
+		dashTimer += Time.deltaTime;
+		if (dashTimer >= dashDuration) fsm.ChangeToState(STATE_IDLE);
+
+		transform.Translate(direction * dashSpeed * Time.deltaTime);
+	}
+
+	private void ExitDashState()
+	{
+		isDashing = false;
+		dashAudioSource.Stop();
+	}
+
+	#endregion
+
+	#region STATE_SHOOT
+
+	private void EnterShootState()
 	{
 		shootInput = false;
 		isShooting = true;
+		StartCoroutine(
+			Shoot(() => fsm.ChangeToState(STATE_MOVE))
+		);
+	}
+
+	private IEnumerator Shoot(Action onEnd)
+	{
 		Vector2 direction = (aimInput - (Vector2)transform.position).normalized;
 		var hits = Physics2D.Raycast(transform.position, direction, hitFilter, hitBuffer);
 		var endPoint = hits > 0
@@ -156,23 +193,14 @@ public class PlayerController : MonoBehaviour
 		shootAudioSource.Play();
 		yield return new WaitForSeconds(0.25f);
 		beam.enabled = false;
-		isShooting = false;
-		StartCoroutine(IdleState());
+
+		onEnd?.Invoke();
 	}
 
-	private IEnumerator Shoot(Vector2 direction)
+	private void ExitShootState()
 	{
-		var hits = Physics2D.Raycast(transform.position, direction, hitFilter, hitBuffer);
-		var endPoint = hits > 0
-			? (Vector3)hitBuffer[0].point
-			: transform.position + (Vector3)direction * 100;
-		var beamPositions = new Vector3[]{
-				transform.position,
-				endPoint
-			};
-		beam.SetPositions(beamPositions);
-		beam.enabled = true;
-		yield return new WaitForSeconds(0.25f);
-		beam.enabled = false;
+		isShooting = false;
 	}
+
+	#endregion
 }
---
+++ Assets/Player/StateMachine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+
+	private Dictionary<int, State> states;
+	private State state;
+	private State nextState;
+
+	public StateMachine()
+	{
+		states = new Dictionary<int, State>();
+	}
+
+	public void Update()
+	{
+		if (nextState != null)
+		{
+			state?.OnExit?.Invoke();
+			state = nextState;
+			nextState = null;
+			state?.OnEnter?.Invoke();
+		}
+		state?.OnUpdate?.Invoke();
+	}
+
+	public void AddState(int id, Action onEnter, Action onUpdate, Action onExit)
+	{
+		states[id] = new State(onEnter, onUpdate, onExit);
+	}
+
+	public void ChangeToState(int id)
+	{
+		nextState = states[id];
+	}
+}
+
+public class State
+{
+	public Action OnEnter { get; private set; }
+	public Action OnUpdate { get; private set; }
+	public Action OnExit { get; private set; }
+
+	public State(Action onEnter, Action onUpdate, Action onExit)
+	{
+		OnEnter = onEnter;
+		OnUpdate = onUpdate;
+		OnExit = onExit;
+	}
+}
