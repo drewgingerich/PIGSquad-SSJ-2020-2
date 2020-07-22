280e76aa5134c0df99dfd5bca51fb70af99a82dd
Drew Gingerich <gingerich93@gmail.com>
2020-07-21 22:44:30 -0700

    Add yield instruction to wait for note

---
+++ Assets/Audio/WaitForNote.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class WaitForNote : CustomYieldInstruction
+{
+	private Note noteType;
+	private double targetTime;
+	private int offset;
+
+	public override bool keepWaiting
+	{
+		get
+		{
+			return targetTime < AudioSettings.dspTime;
+		}
+	}
+
+	public WaitForNote(Note noteType, int offset = 0)
+	{
+		this.noteType = noteType;
+		targetTime = NoteTracker.GetNextNoteTime(noteType, offset);
+	}
+}
