7bfc9c868d337e7e9ba6239bd3eaa6a9c3b09aca
Drew Gingerich <gingerich93@gmail.com>
2020-07-24 12:00:08 -0700

    Add WaitForTick custom yield
    To wait for a specific tick in a bar

---
+++ Assets/Audio/WaitForTick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class WaitForTick : CustomYieldInstruction
+{
+	private double remainingTicks;
+
+	public override bool keepWaiting
+	{
+		get
+		{
+			return remainingTicks > 0;
+		}
+	}
+
+	public WaitForTick(int tick)
+	{
+		remainingTicks = tick;
+		remainingTicks -= Conductor.tickCount;
+		Metronome.OnTick += HandleTick;
+
+	}
+
+	private void HandleTick(int count)
+	{
+		remainingTicks -= count;
+		if (remainingTicks <= 0)
+		{
+			Metronome.OnTick -= HandleTick;
+		}
+	}
+}
