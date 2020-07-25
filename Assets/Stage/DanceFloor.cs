14fd6429c5164ef696ece44e2949111149c1e1a0
Drew Gingerich <gingerich93@gmail.com>
2020-07-24 20:55:59 -0700

    Add fun floor and wall effects

---
+++ Assets/Stage/DanceFloor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DanceFloor : MonoBehaviour
+{
+	[SerializeField]
+	GameObject floor1;
+	[SerializeField]
+	GameObject floor2;
+
+	private void Start()
+	{
+		MusicStartAnnouncer.OnStart += StartDiscoTime;
+	}
+
+	private void StartDiscoTime(double startTime)
+	{
+		MusicStartAnnouncer.OnStart -= StartDiscoTime;
+		StartCoroutine(DiscoTimeRoutine());
+	}
+
+	private IEnumerator DiscoTimeRoutine()
+	{
+		while (true)
+		{
+			yield return new WaitForNote(Note.Quarter);
+			floor1.SetActive(true);
+			floor2.SetActive(false);
+			yield return new WaitForNote(Note.Quarter);
+			floor1.SetActive(false);
+			floor2.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Stage/WallPulse.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WallPulse : MonoBehaviour
+{
+
+	[SerializeField]
+	private new Tilemap tilemap;
+	[SerializeField]
+	private Color color1;
+	[SerializeField]
+	private Color color2;
+
+	void Start()
+	{
+		MusicStartAnnouncer.OnStart += StartPulse;
+
+	}
+
+	private void StartPulse(double startTime)
+	{
+		MusicStartAnnouncer.OnStart -= StartPulse;
+		StartCoroutine(PulseRoutine());
+
+	}
+
+	private IEnumerator PulseRoutine()
+	{
+		var startColor = color1;
+		var endColor = color2;
+		while (true)
+		{
+			var targetTime = Conductor.GetNextNote(Note.Half);
+			yield return StartCoroutine(ColorLerp(startColor, endColor, targetTime));
+			var temp = startColor;
+			startColor = endColor;
+			endColor = temp;
+		}
+
+	}
+
+	private IEnumerator ColorLerp(Color startColor, Color endColor, double targetTime)
+	{
+		var startTime = AudioSettings.dspTime;
+		var totalDiff = targetTime - startTime;
+		double progress = 0f;
+		while (progress < 1)
+		{
+			var curDiff = AudioSettings.dspTime - startTime;
+			progress = curDiff / totalDiff;
+			tilemap.color = Color.Lerp(startColor, endColor, (float)progress);
+			yield return null;
+		}
+	}
+}
