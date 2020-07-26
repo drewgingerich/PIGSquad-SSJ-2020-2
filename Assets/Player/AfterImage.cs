7e833ce170f261695ef9a2b814a7001d158350ea
Drew Gingerich <gingerich93@gmail.com>
2020-07-25 17:47:32 -0700

    Rename dah controller to dashvfx

--- Assets/Player/AfterImage.cs
+++ Assets/Player/AfterImage.cs
@@ -2,8 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class AfterImage : MonoBehaviour
 {
+	private Vector2 direction;
+	private float speed;
+	private Note duration;
+
+	public void Go(Vector2 direction, float speed, Note duration)
+	{
+		this.direction = direction;
+		this.speed = speed;
+		this.duration = duration;
+	}
+
+	// public IEnumerator GoRoutine()
+	// {
+	// 	dashController.Activate(direction, dashDistance);
+	// 	rb.velocity = direction * dashSpeed;
+	// 	dashAudioSource.SetScheduledEndTime(Conductor.CalcNoteTime(dashNoteType));
+	// 	yield return new WaitForNoteLength(dashNoteType);
+
+	// 	rb.velocity = direction * moveSpeed;
+	// 	fsm.ChangeToState(STATE_MOVE);
+	// 	yield return null;
+	// }
+
 	private void Awake()
 	{
 		Destroy(gameObject, 0.25f);
---
+++ Assets/Player/AfterimageVfx.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AfterimageVfx : MonoBehaviour
+{
+
+	[SerializeField]
+	private GameObject afterImagePrefab;
+	[SerializeField]
+	private int afterImageCount = 4;
+	[SerializeField]
+	private Note destroyDelay = Note.Eighth;
+
+	private Vector2 direction;
+	private float distance;
+	private float time;
+
+	public void CreateAfterimages(Vector2 direction, float distance, float time)
+	{
+		this.direction = direction;
+		this.distance = distance;
+		this.time = time;
+		StartCoroutine(AfterimageRoutine());
+	}
+
+	public IEnumerator AfterimageRoutine()
+	{
+		yield return CreateAfterimages();
+		yield return new WaitForNote(destroyDelay);
+		yield return DestroyAfterimages();
+	}
+
+	public IEnumerator CreateAfterimages()
+	{
+		var spawnPosition = (Vector2)transform.position;
+		var spacing = distance / (afterImageCount - 1);
+		// Making afterimage spawn a bit ahead looks cooler!
+		// spawnPosition += direction * spacing;
+		var pause = (float)Conductor.noteDurations[Note.Thirtysecond];
+		for (int i = 0; i < afterImageCount; i++)
+		{
+			Instantiate(afterImagePrefab, spawnPosition, Quaternion.identity);
+			spawnPosition += direction * spacing;
+			yield return new WaitForSeconds(pause);
+		}
+	}
+
+	public IEnumerator DestroyAfterimages()
+	{
+		PlayerSfx.PlayDashSfx();
+		yield return new WaitForNote(Note.Thirtysecond);
+	}
+}
