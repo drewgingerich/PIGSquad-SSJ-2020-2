56dd4571e3095f13b6008571078f194d779e9824
Drew Gingerich <gingerich93@gmail.com>
2020-07-25 17:44:31 -0700

    Add custom yield to wait for dsp time

---
+++ Assets/Audio/WaitForDspTime.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class WaitForDspTime : CustomYieldInstruction
+{
+	private double targetTime;
+
+	public override bool keepWaiting
+	{
+		get
+		{
+			return AudioSettings.dspTime < targetTime;
+		}
+	}
+
+	public WaitForDspTime(double time)
+	{
+		targetTime = time;
+	}
+
+}
