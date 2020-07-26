f0247d0c0191b097c2b509d9e8c8dad3cb39a213
Drew Gingerich <gingerich93@gmail.com>
2020-07-25 23:40:51 -0700

    Happy with the dash fx

--- Assets/Player/AfterImage.cs
+++ Assets/Player/AfterImage.cs
@@ -2,20 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class Afterimage : MonoBehaviour
 {
-	private double targetTime;
+	[SerializeField]
+	private Transform ring;
+	[SerializeField]
+	private float startScale = 2;
+	[SerializeField]
+	private float endScale = 1;
+
+	private double startTime;
+	private double currentTime;
+	private double targetDiff;
 
 	public void Run(double targetTime)
 	{
-		this.targetTime = targetTime;
+		startTime = currentTime = AudioSettings.dspTime;
+		targetDiff = targetTime - currentTime;
 		StartCoroutine(VfxRoutine());
 	}
 
 	private IEnumerator VfxRoutine()
 	{
-		yield return new WaitForDspTime(targetTime);
+		double diff = 0;
+		while (diff < targetDiff)
+		{
+			diff = currentTime - startTime;
+			var progress = (float)(diff / targetDiff);
+			float scale = Mathf.Lerp(startScale, endScale, progress);
+			ring.localScale = Vector3.one * scale;
+
+			currentTime = AudioSettings.dspTime;
+
+			yield return null;
+		}
 		Destroy(gameObject);
 	}
 }
