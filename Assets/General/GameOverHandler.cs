8919e63c603413ad3976483ea71ff5799612b578
Drew Gingerich <gingerich93@gmail.com>
2020-07-26 17:39:23 -0700

    Add game over menu and logic

---
+++ Assets/General/GameOverHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameOverHandler : MonoBehaviour
+{
+	[SerializeField]
+	GameObject gameOverMenu;
+
+	void Awake()
+	{
+		PlayerController.OnPlayerDeath += HandleGameOver;
+	}
+
+	void HandleGameOver()
+	{
+		PlayerController.OnPlayerDeath -= HandleGameOver;
+		gameOverMenu.SetActive(true);
+	}
+}
